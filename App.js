89a6a8632b90058bdb18ba9720693d3d40c793b1
Chinedum Ukejianya <cukejianya@gmail.com>
2022-07-01 18:02:44 -0400

    Add basic note text

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  const [text, onChangeText] = React.useState('');
+  const [number, onChangeNumber] = React.useState(null);
+ 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Today's Highlight:</Text> <TextInput onChangeText={onChangeText} value={text} />
+      <Text>Did I make Time for it? Yes! No</Text>
+      <Text>Focus: 4</Text>
+      <Text>Energy: 7</Text>
+      <Text>Moment I'm greatful for:</Text>
       <StatusBar style="auto" />
     </View>
   );
